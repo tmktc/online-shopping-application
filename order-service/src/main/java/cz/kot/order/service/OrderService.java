64c99c951213e263c54da8aa0ca7b6c2c2963545
tmktc <tmktc.git@gmail.com>
2024-06-30 11:21:44 +0200

    order-service: add OrderService

---
+++ order-service/src/main/java/cz/kot/order/service/OrderService.java
@@ -0,0 +1,26 @@
+package cz.kot.order.service;
+
+import cz.kot.order.dto.OrderRequest;
+import cz.kot.order.model.Order;
+import cz.kot.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setPrice(orderRequest.price());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
+}
