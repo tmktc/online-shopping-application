9e3db5f95321320c3549c6d1cdadcf33b9280a6a
tmktc <tmktc.git@gmail.com>
2024-07-02 20:03:12 +0200

    add inventory-service module

---
+++ inventory-service/src/test/java/cz/kot/inventory/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package cz.kot.inventory;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
