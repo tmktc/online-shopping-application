73cda8cd2867f9693f8048e40b892eec51e557b3
tmktc <tmktc.git@gmail.com>
2024-07-03 21:50:14 +0200

    add synchronous communication between order-service and inventory-service

--- order-service/src/main/java/cz/kot/order/service/OrderService.java
+++ order-service/src/main/java/cz/kot/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cz.kot.order.service;
 
+import cz.kot.order.client.InventoryClient;
 import cz.kot.order.dto.OrderRequest;
 import cz.kot.order.model.Order;
 import cz.kot.order.repository.OrderRepository;
@@ -16,6 +17,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     /**
      * Receives requests from the controller to place orders.
@@ -24,12 +26,17 @@ public class OrderService {
      * @param orderRequest order requested for placement
      */
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
