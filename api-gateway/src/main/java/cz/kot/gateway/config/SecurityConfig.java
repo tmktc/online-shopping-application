3acd379621a96bd28c455fa34f782087e79985e7
tmktc <tmktc.git@gmail.com>
2024-07-05 15:26:35 +0200

    api-gateway: add SecurityConfig class for Keycloak

---
+++ api-gateway/src/main/java/cz/kot/gateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package cz.kot.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Config class for Keycloak
+ */
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+                .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
