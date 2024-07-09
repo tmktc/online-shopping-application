98fbc0138fc25f2e71603e1d1cf6490bffc44c8d
tmktc <tmktc.git@gmail.com>
2024-07-09 19:45:12 +0200

    notification-service: setup email sending

---
+++ notification-service/src/main/java/cz/kot/order/event/OrderPlacedEvent.java
@@ -0,0 +1,13 @@
+package cz.kot.order.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+    private String email;
+}
